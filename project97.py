8c0a1ca45d93878aecc1522a4b559fe37adb51fb
HypertextAssassin <72544486+HypertextAssassin69@users.noreply.github.com>
2022-03-29 15:17:21 +0530

    Add files via upload

---
+++ project97.py
@@ -0,0 +1,23 @@
+import random
+i = random.randint(1,9)
+print(i)
+chance = 5
+
+print("you have got ",chance," chances left, use them wisly")
+userGuess = int(input("Please Enter Ur Guess: "))
+while chance > 1 :
+    chance -=1
+    if userGuess>i:
+        print("you have got ",chance," chances left, use them wisly")
+        print("Ur Guess is Bigger Than The No. Please put a number smaller than ",userGuess)
+        userGuess = int(input("Please Enter Another Guess: "))
+    elif userGuess<i:
+        print("you have got ",chance," chances left, use them wisly")
+        print("Ur Guess is Smaller Than The No. Please put a number greater than ",userGuess)
+        userGuess = int(input("Please Enter Another Guess: "))
+if userGuess == i:
+    print("WWWWOOOOWWWWW!!!! u guessed the number perfectly. u will be a great hacker someday.")
+    chance = 3   
+
+if  not chance > 1:
+    print("you lose!!!!!!! and you won't become any sort of hacker in future.")
